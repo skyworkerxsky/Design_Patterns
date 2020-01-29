c5fcfa64de0931f8cfaa56f5a5a8c508c8cba911
skyworkerxsky <pwuser@yandex.ru>
2020-01-29 13:40:13 +0500

    add decorator

---
+++ 3 Decorator/MyPlayground.playground/Contents.swift
@@ -0,0 +1,69 @@
+import UIKit
+
+
+protocol Porsche {
+    func getPrice() -> Double
+    func getDescription() -> String
+}
+
+class Boxter: Porsche {
+    
+    func getPrice() -> Double {
+        return 120
+    }
+    
+    func getDescription() -> String {
+        return "Porsche Boxter"
+    }
+    
+}
+
+class PorscheDecorator: Porsche {
+
+    private let decoratedPorsche: Porsche
+    
+    required init(dp: Porsche) {
+        self.decoratedPorsche = dp
+    }
+    
+    func getPrice() -> Double {
+        return decoratedPorsche.getPrice()
+    }
+    
+    func getDescription() -> String {
+        return decoratedPorsche.getDescription()
+    }
+    
+}
+
+class PremiumAudioSystem: PorscheDecorator {
+    
+    required init(dp: Porsche) {
+        super.init(dp: dp)
+    }
+    
+    override func getPrice() -> Double {
+        return super.getPrice() + 30
+    }
+    
+    override func getDescription() -> String {
+        return super.getDescription() + " with premium audio system"
+    }
+    
+}
+
+class PanoramicSunroof: PorscheDecorator {
+    
+    required init(dp: Porsche) {
+        super.init(dp: dp)
+    }
+    
+    override func getPrice() -> Double {
+        return super.getPrice() + 100
+    }
+    
+    override func getDescription() -> String {
+        return super.getDescription() + " with panoramic sunroof"
+    }
+
+}
